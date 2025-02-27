c5d83a7e0be75e0e5a18dbb350ea4d6d13fb4bab
jisha <kumarjishas@gmail.com>
2025-02-27 12:56:02 -0500

    commit

--- src/main/java/org/example/java8/SortE.java
+++ src/main/java/org/example/java8/SortE.java
@@ -3,6 +3,7 @@ package org.example.java8;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SortE {
@@ -15,7 +16,7 @@ public class SortE {
                 new Employee(1.8, "Jisha","M"),
                 new Employee(2.0, "Sum","C"),
                 new Employee(1.2, "Juv","A"),
-                new Employee(2.4, "Juv2","C"));
+                new Employee(1.2, "Juv2","C"));
         System.out.println("\nSorted by rating:");
         List<Employee> sorted = empList.stream().sorted(Comparator.comparing(Employee::getRating))
                 .collect(Collectors.toList());
@@ -24,6 +25,10 @@ public class SortE {
         List<Employee> empList1 = empList.stream().
                 sorted(Comparator.comparing(Employee::getEmpName)).collect(Collectors.toList());
         System.out.println(empList1);
+
+        Map<Double, List<Employee>> hmap = empList.stream().collect(Collectors.groupingBy(Employee::getRating));
+        System.out.println("emp Group By Rating ");
+        System.out.println(hmap);
     }
 
 }
